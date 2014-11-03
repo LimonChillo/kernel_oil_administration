5368d77c44b9da8998fea60c2fddc6c267b03860
Fabian Hoffmann <fhoffmann.mmt-b2013@fh-salzburg.ac.at>
2014-11-03 17:41:35 +0100

    head, index, footer

---
+++ head.php
@@ -0,0 +1,22 @@
+<?php  include "functions.php"; ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<title>KernOil</title>
+	<link rel="stylesheet" href="style/main.css">
+	
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+	<script src="js/functions.js"></script>
+</head>
+<body>
+
+
+<?php
+	include "config.php";
+	$dbh = new PDO($DSN, $DB_USER, $DB_PASS);
+
+    $pagetitle = "KernOil";
+    
+?>
