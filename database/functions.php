9711075db030fe9b700957c5a6c97162504eb659
Simon Hintersonnleitner <simon@Simons-MacBook-Pro-2.local>
2014-11-13 18:20:28 +0100

    removed

--- database/functions.php
+++ database/functions.php
@@ -8,8 +8,7 @@ $dbh = connectToDB();
 function connectToDB()
 {
 
-  $root = realpath($_SERVER["DOCUMENT_ROOT"]);
-  include "$root/kernoil/database/config.php";
+  include "database/config.php";
   if( ! $DB_NAME ) die('please create config.php, define $DB_NAME, $DB_USER, $DB_PASS there');
   try {
       $dbh = new PDO($DSN, $DB_USER, $DB_PASS);
