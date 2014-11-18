cf46b5608962313a4b40c33e1dd544e9bfe29eb5
Simon Hintersonnleitner <simon@c78-104-171-188.fh-salzburg.ac.at>
2014-11-18 14:36:09 +0100

    fixed link

--- barrels.php
+++ barrels.php
@@ -1,7 +1,7 @@
 <?php include "head.php";?>
 <div class="container">
 	<h1>Fässer</h1>
-	<a href="addBarrols.php" class="btn btn-default">Fässer hinzufügen</a>
+	<a href="addBarrels.php" class="btn btn-default">Fässer hinzufügen</a>
 	<a href="#" class="btn btn-default">gewählte Fässer pressen</a>
 	</p>
 	<table class="table table-hover">
