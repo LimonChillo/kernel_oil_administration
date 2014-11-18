b5264d952c7949a7f91bd09d25d4242deb672785
Simon Hintersonnleitner <simon@c78-104-170-16.fh-salzburg.ac.at>
2014-11-18 11:40:40 +0100

    rename fils

---
+++ barrels.php
@@ -0,0 +1,32 @@
+<?php include "head.php";?>
+<div class="container">
+	<h1>Fässer</h1>
+	<a href="addBarrols.php" class="btn btn-default">Fässer hinzufügen</a>
+	<a href="#" class="btn btn-default">gewählte Fässer pressen</a>
+	</p>
+	<table class="table table-hover">
+  		<tr>
+  			<th>#</th>
+  			<th>Fassnummer</th>
+  			<th>Sorte</th>
+  			<th>Füllstand</th>
+  			<th>Datum</th>
+  		</tr>
+  		<tr>
+  			<td><input type="checkbox"></td>
+  			<td>001</td>
+  			<td>Beispielsorte1</td>
+  			<td>100%</td>
+  			<td>18.11.14</td>
+  		</tr>
+  		<tr>
+  			<td><input type="checkbox"></td>
+  			<td>002</td>
+  			<td>Beispielsorte2</td>
+  			<td>75%</td>
+  			<td>15.11.14</td>
+  		</tr>
+	</table>
+	</div>
+</div>
+<?php include "footer.php";?>
