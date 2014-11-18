5f41978a315e545126aa5af13acdf3475494e85d
Simon Hintersonnleitner <simon@Simons-MacBook-Pro-2.local>
2014-11-18 17:20:16 +0100

    moves fuctions.php call

--- head.php
+++ head.php
@@ -1,4 +1,8 @@
 <?php session_start(); ?>
+
+<?php
+      include "database/functions.php";
+?>
 <!DOCTYPE html>
 <html>
   <head>
@@ -36,7 +40,4 @@
           </div><!--/.nav-collapse -->
         </div>
       </nav>
-      <?php
-        include "database/functions.php";
-        $pagetitle = "KernOil";
-      ?>
+     
