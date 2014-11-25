f655ab66197cff0ce9d83156f1ae4406c1172d4c
Simon Hintersonnleitner <shintersonnleitner.mmt-b2013@fh-salzburg.ac.at>
2014-11-25 18:25:09 +0100

    fixed js

--- addBotteling.php
+++ addBotteling.php
@@ -6,24 +6,18 @@ include "head.php";
 <script type="text/javascript">
 function changeAmount()
 {
+	console.log("yolo");
 	var res = event.target.name.split("_");
 	var inputName = 'count_'+res[1]+'_amount';
 	var input = $("input[name="+inputName+"]");
 
-	var ml = $("input[name='ml']").first().text();
-	alert(ml);
+	var ml = $("input[name='ml']").eq(event.target.id).val();
+
 	ml = parseInt(ml);
 
 	var value = event.target.value;
-
-
 	value = parseInt(value);
-
-
-	alert(ml + " " + value );
-
-	input.val(value * ml);
-
+	input.val(parseFloat(value * ml/1000) + " l");
 
 }
 
@@ -56,20 +50,20 @@ function changeAmount()
 			<label  class="col-sm-4 control-label">Abfüllung in die einzelnen Flaschen</label>
 		</div>
 		<?php $amountPerBottleType =  $pressing->amount*1000 / getAmountOfBottleTypes()?>
-		<?php foreach ($allBottels as $bottle):?>
+		<?php $form_id = 0; foreach ($allBottels as $bottle): ?>
 		<div class="form-group">
 			<div class="col-sm-offset-0 col-sm-2">
-				<input type="text" class="form-control" value='<?php echo $bottle->ml; ?>' name="ml" readonly>
+				<input type="text" class="form-control" value='<?php echo $bottle->ml; ?>' name="ml">
 			</div>
 			<div class=" col-sm-1">
-				<input type="text" onKeyDown="changeAmount()" class="form-control" value='<?php echo $amountPerBottleType/$bottle->ml; ?>' name="count_<?php echo $bottle->ml; ?>">
+				<input type="text" onKeyUp="changeAmount()" id="<?php echo $form_id; ?>" class="form-control" value='<?php echo $amountPerBottleType/$bottle->ml; ?>' name="count_<?php echo $bottle->ml; ?>">
 			</div>
 			<div class=" col-sm-1">
-				<input type="text" class="form-control" name="count_<?php echo $bottle->ml; ?>_amount">
+				<input type="text" class="form-control" name="count_<?php echo $bottle->ml;?>_amount"  value="<?php echo ($amountPerBottleType / 1000 ).' l'; ?>" readonly>
 			</div>
+				<input type="hidden"  name="bottle_id" value="<?php echo  $form_id; ?>">
 		</div>
-		<?php endforeach;?>
-
+		<?php $form_id ++; endforeach;?>
 		<div class="form-group">
 			<div class="col-sm-offset-3 col-sm-1">
 				<button type="submit" name="insertBotteling" class="btn btn-default">Abfüllen</button>
