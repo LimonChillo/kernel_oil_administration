2bac27df639832191838e145517c7796d76291c2
Fabian Johannes Hoffmann <fhoffmann.mmt-b2013@fh-salzburg.ac.at>
2015-02-11 21:44:25 +0100

    add new Footer with links to crafters

--- footer.php
+++ footer.php
@@ -1,8 +1,14 @@
-  
+
 <div class="container">
-  <footer> 
-  	David Kranewitter | Fabian Hoffmann | Simon Hintersonnleitner
+  <footer>
+    <p>Handcrafted by:&nbsp;</p>
+    <div id='crafters'>
+      <a target="_blank" href="https://github.com/RaketenWirt"><div id='kra'>Kra</div></a>
+      <a target="_blank" href="http://www.hintersonnleitner.me"><div id='hi'>Hi</div></a>
+      <a target="_blank" href="http://www.fabianhoffmann.io"><div id='ho'>Ho</div></a>
+    </div>
   </footer>
   </div>
 </body>
+<script> (function() { crafters(); })()</script>
 </html>
--- js/functions.js
+++ js/functions.js
@@ -6,12 +6,31 @@
       url: "translate.php",
       data: "text=" + this.innerHTML,
       success: function(html) {
-          if (html != null) {
-              if(html != " belastung")
-                self.html(html);
-          }
+        if (html != null) {
+          if(html != " belastung")
+            self.html(html);
+        }
       }
     });
   });
   return false;
+}
+
+function crafters() {
+  $('#kra').mouseenter(function() {
+    $('#kra').html('Kranewitter');
+    $('#ho').html('Ho');
+  })
+  $('#hi').mouseenter(function() {
+    $('#hi').html('Hintersonnleitner');
+  })
+  $('#ho').mouseenter(function() {
+    $('#ho').html('Hoffmann');
+    $('#kra').html('Kra');
+  })
+  $('#crafters').mouseleave(function() {
+    $('#kra').html('Kra');
+    $('#hi').html('Hi');
+    $('#ho').html('Ho');
+  })
 }
